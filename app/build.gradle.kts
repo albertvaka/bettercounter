eeb07ca7c57fd12e893171ec9384902656256689
Albert Vaca Cintora <albertvaka@gmail.com>
2023-11-20 22:20:49 +0100

    Bump deps and target sdk, use kotlin for gradle, use libs catalog

---
+++ app/build.gradle.kts
@@ -0,0 +1,79 @@
+buildscript {
+    dependencies {
+        classpath(libs.android.gradlePlugin)
+        classpath(libs.kotlin.gradlePlugin)
+    }
+}
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.google.devtools.ksp)
+}
+
+android {
+    namespace = "org.kde.bettercounter"
+    compileSdk = 34
+    defaultConfig {
+        applicationId = "org.kde.bettercounter"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 30001
+        versionName = "3.0.1"
+
+        javaCompileOptions {
+            annotationProcessorOptions {
+                argument("room.schemaLocation", "$projectDir/schemas".toString())
+            }
+        }
+    }
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+    }
+    compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    androidResources {
+        generateLocaleConfig = true
+    }
+    signingConfigs {
+        getByName("debug") {
+            storeFile = file("debug.keystore")
+            storePassword = "android"
+            keyAlias = "androiddebugkey"
+            keyPassword = "android"
+        }
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+        getByName("debug") {
+            signingConfig = signingConfigs.getByName("debug")
+            applicationIdSuffix = ".debug"
+            versionNameSuffix = " Dev"
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.douglasjunior.android.simple.tooltip)
+    implementation(libs.philJay.mpAndroidChart)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
+    implementation(libs.androidx.recyclerview)
+    implementation(libs.material)
+    coreLibraryDesugaring(libs.android.desugarJdkLibs) // Chrono.UNITS for Android < 26
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,8 +10,11 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url = "https://jitpack.io" }
+        maven {
+            url = uri("https://jitpack.io")
+        }
     }
 }
 rootProject.name = "BetterCounter"
-include ':app'
+
+include(":app")
