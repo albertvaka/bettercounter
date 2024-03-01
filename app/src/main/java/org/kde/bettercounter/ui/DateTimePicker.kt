39341cbe87c9d87fd309bc70eeb6e34a891f46d9
Albert Vaca Cintora <albertvaka@gmail.com>
2024-03-01 19:26:12 +0100

    Split showDatePicker out of showDateTimePicker

--- app/src/main/java/org/kde/bettercounter/ui/DateTimePicker.kt
+++ app/src/main/java/org/kde/bettercounter/ui/DateTimePicker.kt
@@ -6,17 +6,24 @@ import android.content.Context
 import java.util.Calendar
 
 fun showDateTimePicker(context: Context, initialDateTime: Calendar, callback: (Calendar) -> Unit) {
-    val initialYear = initialDateTime.get(Calendar.YEAR)
-    val initialMonth = initialDateTime.get(Calendar.MONTH)
-    val initialDay = initialDateTime.get(Calendar.DAY_OF_MONTH)
     val initialHour = initialDateTime.get(Calendar.HOUR_OF_DAY)
     val initialMinute = initialDateTime.get(Calendar.MINUTE)
     val use24HourClock = true
-    DatePickerDialog(context, { _, year, month, day ->
+    showDatePicker(context, initialDateTime) { cal ->
         TimePickerDialog(context, { _, hour, minute ->
-            val cal = Calendar.getInstance()
-            cal.set(year, month, day, hour, minute)
+            cal.set(Calendar.MINUTE, minute)
+            cal.set(Calendar.HOUR_OF_DAY, hour)
             callback(cal)
         }, initialHour, initialMinute, use24HourClock).show()
+    }}
+
+fun showDatePicker(context: Context, initialDateTime: Calendar, callback: (Calendar) -> Unit) {
+    val initialYear = initialDateTime.get(Calendar.YEAR)
+    val initialMonth = initialDateTime.get(Calendar.MONTH)
+    val initialDay = initialDateTime.get(Calendar.DAY_OF_MONTH)
+    DatePickerDialog(context, { _, year, month, day ->
+        val cal = Calendar.getInstance()
+        cal.set(year, month, day)
+        callback(cal)
     }, initialYear, initialMonth, initialDay).show()
 }
