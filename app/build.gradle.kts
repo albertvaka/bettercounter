6715644b6fa1f8c81b28a433f31bc89c36a7468b
Albert Vaca Cintora <albertvaka@gmail.com>
2024-04-18 00:33:37 +0200

    Relase 4.6.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "org.kde.bettercounter"
         minSdk = 21
         targetSdk = 34
-        versionCode = 40601
-        versionName = "4.6.1"
+        versionCode = 40602
+        versionName = "4.6.2"
 
         javaCompileOptions {
             annotationProcessorOptions {
