f8dcabb9a27d8facd35d6958d1305600b230e21e
Albert Vaca Cintora <albertvaka@gmail.com>
2025-07-02 09:25:31 +0200

    Minor changes

--- app/src/main/java/org/kde/bettercounter/persistence/CounterSummary.kt
+++ app/src/main/java/org/kde/bettercounter/persistence/CounterSummary.kt
@@ -19,13 +19,17 @@ class CounterSummary(
         return if (lastEntry != null && lastEntry > now) lastEntry else now
     }
 
+    fun hasGoal(): Boolean {
+        return goal > 0
+    }
+
     fun isGoalMet(): Boolean {
         return goal in 1..lastIntervalCount
     }
 
     fun getFormattedCount(): CharSequence = buildString {
         append(lastIntervalCount)
-        if (goal > 0) {
+        if (hasGoal()) {
             append('/')
             append(goal)
         }
