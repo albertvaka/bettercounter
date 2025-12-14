c5f5a53ca43b58554363f1ecdf04e98db02ab193
Albert Vaca Cintora <albertvaka@gmail.com>
2025-12-14 16:50:01 +0100

    Fix bug when first hour of day > current hour

--- app/src/main/java/org/kde/bettercounter/extensions/CalendarExtension.kt
+++ app/src/main/java/org/kde/bettercounter/extensions/CalendarExtension.kt
@@ -13,7 +13,12 @@ fun Calendar.truncated(field: Int): Calendar {
     if (field == Calendar.MINUTE) return cal
     cal.set(Calendar.MINUTE, 0)
     if (field == Calendar.HOUR_OF_DAY || field == Calendar.HOUR) return cal
-    cal.set(Calendar.HOUR_OF_DAY, FirstHourOfDay.get())
+    val firstHourOfDay = FirstHourOfDay.get();
+    if (firstHourOfDay > cal.get(Calendar.HOUR_OF_DAY)) {
+        cal.add(Calendar.DAY_OF_YEAR, -1)
+    }
+    cal.set(Calendar.HOUR_OF_DAY, firstHourOfDay)
+    cal.debugToSimpleDateString().andLog()
     if (field in listOf(Calendar.DATE, Calendar.DAY_OF_WEEK, Calendar.DAY_OF_MONTH, Calendar.DAY_OF_YEAR)) return cal
     if (field in listOf(Calendar.WEEK_OF_YEAR, Calendar.WEEK_OF_MONTH)) {
         val dow = cal.get(Calendar.DAY_OF_WEEK)
